efa75734282d38340cabe48ef44043ab3d3a81e3
Chris Griffis <chris.griffis@insitu.com>
2018-04-17 16:35:28 -0700

    tighten up sorting alg source with template aliases.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 g++ -std=c++14 static_algs_main.cpp -o static_algs.out 2> run.log
-echo '\n\n++++++++++++++++++++++++++++++++++++++++++++++++++\n\nProgram output:\n\n' >> run.log
+echo '>>> stderr output:' >> run.log
+echo >> run.log
+echo '>>> Program output:' >> run.log
+echo >> run.log
 ./static_algs.out >> run.log
 cat run.log
--- static_algs_main.cpp
+++ static_algs_main.cpp
@@ -1,107 +1,95 @@
-#include <iostream>
-#include <functional>
-#include <vector>
-using namespace std;
+#include <iostream> 
+#include <functional> //for std::conditional
+#include <vector> //for debug output
 
 namespace static_alg
 {
     //natural number sequence
-    template<size_t... Is>
+    template<std::size_t... Is>
     class Seq {};
 
     //print sequence metadata
-    template<size_t... Is>
+    template<std::size_t... Is>
     void print(Seq<Is...>)
     {
-        vector<size_t> v{ Is... };
-        for (auto e : v) cout << e << " ";
-        cout << endl;
+        std::vector<std::size_t> v{ Is... };
+        for (auto e : v) std::cout << e << " ";
+        std::cout << std::endl;
     }
 
     //concatenate two sequences
     template<typename T1, typename T2> struct concat;
-    template<size_t... s1n, size_t... s2n>
+    template<typename T1, typename T2> using concat_t = typename concat<T1, T2>::result;
+
+    template<std::size_t... s1n, std::size_t... s2n>
     struct concat<Seq<s1n...>, Seq<s2n...>>
     {
         using result = Seq<s1n..., s2n...>;
     };
 
-    //get the front N subsequence
-    template<size_t N, typename T> struct front;
-    template<size_t N, size_t s1, size_t... s1n>
-    struct front<N, Seq<s1, s1n...>>
+    //get the split a subsequence at N
+    template<std::size_t N, typename T> struct split_after;
+    template<std::size_t N, typename T> using front_t = typename split_after<N, T>::front;
+    template<std::size_t N, typename T> using tail_t = typename split_after<N, T>::tail;
+    template<std::size_t N, std::size_t s1, std::size_t... s1n>
+    struct split_after<N, Seq<s1, s1n...>>
     {
-        using result = typename concat<
-            Seq<s1>, 
-            typename front<N - 1, Seq<s1n...>>::result
-        >::result;
-        using tail = typename front<N - 1, Seq<s1n...>>::tail;
+        using front = concat_t<Seq<s1>, front_t<N - 1, Seq<s1n...>>>;
+        using tail = tail_t<N - 1, Seq<s1n...>>;
     };
-    template< size_t s1, size_t... s1n>
-    struct front<0, Seq<s1, s1n...>>
+    template< std::size_t s1, std::size_t... s1n>
+    struct split_after<0, Seq<s1, s1n...>>
     {
-        using result = Seq<s1>;
+        using front = Seq<s1>;
         using tail = Seq<s1n...>;
-    };
-
-    //split a sequence type at an index
-    template<typename T> struct split;
-    template<size_t... s1n>
-    class split<Seq<s1n...>>
-    {
-        constexpr static size_t mid = (sizeof...(s1n) - 1) / 2;
-    public:
-        using Left = typename front<mid, Seq<s1n...>>::result;
-        using Right = typename front<mid, Seq<s1n...>>::tail;
     };
 
     //merge two sequences
     template<typename T1, typename T2> struct merge;
-    template<size_t s1, size_t... s1n, size_t s2, size_t... s2n>
+    template<typename T1, typename T2> using merge_t = typename merge<T1, T2>::result;
+
+    template<std::size_t s1, std::size_t... s1n, std::size_t s2, std::size_t... s2n>
     struct merge<Seq<s1, s1n...>, Seq<s2, s2n...>>
     {
-        using result = typename concat<
-            typename conditional<
-                (s1 < s2), 
-                Seq<s1>, 
-                Seq<s2>
-            >::type, 
-            typename conditional<
-                (s1 < s2),
-                typename merge<Seq<s1n...>, Seq<s2, s2n...>>::result,
-                typename merge<Seq<s1, s1n...>, Seq<s2n...>>::result
-            >::type
-        >::result;
+    private:
+        constexpr static bool use_left = (s1 < s2);
+    public:
+        //create a new Seq that starts with smallest head,
+        //and merges the remaining sub-Seq with the other 
+        //Seq, which is left unchanged in this pass.
+        using result = concat_t<
+            std::conditional_t<use_left, Seq<s1>, Seq<s2>>,
+            std::conditional_t<use_left,
+            merge_t<Seq<s1n...>, Seq<s2, s2n...>>,
+            merge_t<Seq<s1, s1n...>, Seq<s2n...>>>>;
     };
     //merge specializations
-    template<size_t... s1n> struct merge<Seq<s1n...>, Seq<>> { using result = Seq<s1n...>; };
-    template<size_t... s2n> struct merge<Seq<>, Seq<s2n...>> { using result = Seq<s2n...>; };
+    template<std::size_t... s1n> struct merge<Seq<s1n...>, Seq<>> { using result = Seq<s1n...>; };
+    template<std::size_t... s2n> struct merge<Seq<>, Seq<s2n...>> { using result = Seq<s2n...>; };
     template<> struct merge<Seq<>, Seq<>> { using result = Seq<>; };
 
     //sort
     template<typename T> class sort;
-    template<size_t... s1n>
+    template<typename T> using sort_t = typename sort<T>::result;
+
+    template<std::size_t... s1n>
     class sort<Seq<s1n...>>
     {
-        using S = Seq<s1n...>;
-        using L = typename split<S>::Left;
-        using R = typename split<S>::Right;
+        constexpr static std::size_t mid = (sizeof...(s1n)-1) / 2;
+        using Left = front_t<mid, Seq<s1n...>>;
+        using Right = tail_t<mid, Seq<s1n...>>;
     public:
-        using result = typename merge<
-            typename sort<L>::result,
-            typename sort<R>::result
-        >::result;
+        using result = merge_t<sort_t<Left>, sort_t<Right>>;
     };
-    template<size_t I> class sort<Seq<I>> { public: using result = Seq<I>; };
-    template<typename T>
-    using sorted_t = typename sort<T>::result;
+    //sort specializations
+    template<std::size_t I> class sort<Seq<I>> { public: using result = Seq<I>; };
 }
 
 int main()
 {
     using namespace static_alg;
     using Unsorted = Seq<6, 5, 8, 7, 9, 2, 3, 1, 11>;
-    using Sorted = sorted_t<Unsorted>;
+    using Sorted = sort_t<Unsorted>;
     print(Unsorted{});
     print(Sorted{});
     return 0;
