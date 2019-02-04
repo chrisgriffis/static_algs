13b3bae3bd6652c4f646fca7d86f75b416d634f7
Chris Griffis <chris.griffis@microsoft.com>
2019-02-04 10:21:52 -0800

    made vs sln, commented main, tweaks to sort

--- src/static_sort.h
+++ src/static_sort.h
@@ -1,90 +1,100 @@
 #include <iostream> 
 #include <functional> //for std::conditional
-#include <initializer_list> //for debug output
+#include <vector> //for debug output
 
 namespace my
-{    namespace static_alg
-    {
-        //natural number sequence
-        template<std::size_t... Is>
-        class Seq {};
+{
+	namespace static_alg
+	{
+		//natural number sequence
+		template<std::size_t... Is>
+		class Seq {};
 
-        //print sequence metadata
-        template<std::size_t... Is>
-        void print(Seq<Is...>) { ((std::cout <<  Is << " "), ...) << std::endl; }
+		//print sequence metadata
+		template<std::size_t... Is>
+		void print(Seq<Is...>)
+		{
+			constexpr std::size_t v[] = { Is... };
+			std::cout << std::dec;
+			for (auto e : v) std::cout << e << " ";
+			std::cout << std::endl;
+		}
 
-        //
-        //concatenate two sequences
-        //
-        template<typename T1, typename T2> struct concat;
-        template<typename T1, typename T2> using concat_t = typename concat<T1, T2>::result;
-        template<std::size_t... s1n, std::size_t... s2n>
-        struct concat<Seq<s1n...>, Seq<s2n...>>
-        {
-            using result = Seq<s1n..., s2n...>;
-        };
+		//
+		//concatenate two sequences
+		//
+		template<typename T1, typename T2> struct concat;
+		template<typename T1, typename T2> using concat_t = typename concat<T1, T2>::result;
+		template<std::size_t... s1n, std::size_t... s2n>
+		struct concat<Seq<s1n...>, Seq<s2n...>>
+		{
+			using result = Seq<s1n..., s2n...>;
+		};
 
-        //
-        //split a sequence after zero-based index N into two subsequences
-        //
-        template<std::size_t N, typename T> struct split_after;
-        template<std::size_t N, typename T> using front_t = typename split_after<N, T>::front;
-        template<std::size_t N, typename T> using tail_t = typename split_after<N, T>::tail;
-        template<std::size_t N, std::size_t s1, std::size_t... s1n>
-        struct split_after<N, Seq<s1, s1n...>>
-        {
-            using front = concat_t<Seq<s1>, front_t<N - 1, Seq<s1n...>>>;
-            using tail = tail_t<N - 1, Seq<s1n...>>;
-        };
-        template< std::size_t s1, std::size_t... s1n>
-        struct split_after<0, Seq<s1, s1n...>>
-        {
-            using front = Seq<s1>;
-            using tail = Seq<s1n...>;
-        };
+		//
+		//split a sequence after zero-based index N into two subsequences
+		//
+		template<std::size_t N, typename T> struct split_after;
+		template<std::size_t N, typename T> using front_t = typename split_after<N, T>::front;
+		template<std::size_t N, typename T> using tail_t = typename split_after<N, T>::tail;
+		template<std::size_t N, std::size_t s1, std::size_t... s1n>
+		struct split_after<N, Seq<s1, s1n...>>
+		{
+			using front = concat_t<Seq<s1>, front_t<N - 1, Seq<s1n...>>>;
+			using tail = tail_t<N - 1, Seq<s1n...>>;
+		};
+		template< std::size_t s1, std::size_t... s1n>
+		struct split_after<0, Seq<s1, s1n...>>
+		{
+			using front = Seq<s1>;
+			using tail = Seq<s1n...>;
+		};
 
-        //
-        //merge two sequences
-        //
-        template<typename T1, typename T2> struct merge;
-        template<typename T1, typename T2> using merge_t = typename merge<T1, T2>::result;
-        template<std::size_t s1, std::size_t... s1n, std::size_t s2, std::size_t... s2n>
-        struct merge<Seq<s1, s1n...>, Seq<s2, s2n...>>
-        {
-        private:
-            constexpr static bool use_left = (s1 < s2);
-        public:
-            //create a new Seq that starts with smallest head,
-            //and merges the remaining sub-Seq with the other 
-            //Seq, which is left unchanged in this pass.
-            using result = concat_t<
-                std::conditional_t<use_left, Seq<s1>, Seq<s2>>,
-                std::conditional_t<use_left,
-                merge_t<Seq<s1n...>, Seq<s2, s2n...>>,
-                merge_t<Seq<s1, s1n...>, Seq<s2n...>>>>;
-        };
-        //merge specializations
-        template<std::size_t... s1n> struct merge<Seq<s1n...>, Seq<>> { using result = Seq<s1n...>; };
-        template<std::size_t... s2n> struct merge<Seq<>, Seq<s2n...>> { using result = Seq<s2n...>; };
-        template<> struct merge<Seq<>, Seq<>> { using result = Seq<>; };
+		//
+		//merge two sequences
+		//
+		template<typename T1, typename T2> struct merge;
+		template<typename T1, typename T2> using merge_t = typename merge<T1, T2>::result;
+		template<std::size_t s1, std::size_t... s1n, std::size_t s2, std::size_t... s2n>
+		struct merge<Seq<s1, s1n...>, Seq<s2, s2n...>>
+		{
+		private:
+			constexpr static bool use_left = (s1 < s2);
+			static bool f[use_left ? 1 : -1];
+		public:
+			//create a new Seq that starts with smallest head,
+			//and merges the remaining sub-Seq with the other 
+			//Seq, which is left unchanged in this pass.
+			using result = concat_t<
+				std::conditional_t<use_left, Seq<s1>, Seq<s2>>,
 
-        //
-        //sort
-        //
-        template<typename T> class sort;
-        template<typename T> using sort_t = typename sort<T>::result;
-        template<std::size_t... s1n>
-        class sort<Seq<s1n...>>
-        {
-            constexpr static std::size_t mid = (sizeof...(s1n)-1) / 2;
-            using Left = front_t<mid, Seq<s1n...>>;
-            using Right = tail_t<mid, Seq<s1n...>>;
-        public:
-            using result = merge_t<sort_t<Left>, sort_t<Right>>;
-        };
-        //sort specializations
-        template<std::size_t I> class sort<Seq<I>> { public: using result = Seq<I>; };
-    }
+				typename std::conditional_t<
+				use_left,
+				merge_t<Seq<s1n...>, Seq<s2, s2n...>>,
+				merge_t<Seq<s1, s1n...>, Seq<s2n...>>>>;
+		};
+		//merge specializations
+		template<std::size_t... s1n> struct merge<Seq<s1n...>, Seq<>> { using result = Seq<s1n...>; };
+		template<std::size_t... s2n> struct merge<Seq<>, Seq<s2n...>> { using result = Seq<s2n...>; };
+		template<> struct merge<Seq<>, Seq<>> { using result = Seq<>; };
+
+		//
+		//sort
+		//
+		template<typename T> class sort;
+		template<typename T> using sort_t = typename sort<T>::result;
+		template<std::size_t... s1n>
+		class sort<Seq<s1n...>>
+		{
+			constexpr static std::size_t mid = (sizeof...(s1n) - 1) / 2;
+			using Left = front_t<mid, Seq<s1n...>>;
+			using Right = tail_t<mid, Seq<s1n...>>;
+		public:
+			using result = merge_t<sort_t<Left>, sort_t<Right>>;
+		};
+		//sort specializations
+	template<std::size_t I> class sort<Seq<I>> { public: using result = Seq<I>; };
+	}
 }
 // int main()
 // {
--- static_algs_main.cpp
+++ static_algs_main.cpp
@@ -5,18 +5,16 @@
 int static_sort_main();
 int my_tuple_main();
 
-constexpr struct { int (*_main)(); const char *_name; } labels[] = 
+constexpr struct foo { int (*_main)(); const char *_name; } labels[] = 
 {
     {&static_sort_main,"static_sort_main"},
     {&my_tuple_main,"my_tuple_main"},
 };
-template<typename T, size_t N>
-constexpr size_t count_of(T (&)[N]){return N;}
 
 int main(int argc, char const *argv[])
 {
     using namespace std;
-    constexpr size_t k(count_of(labels));
+    constexpr size_t k(sizeof(labels)/sizeof(foo));
     
     cout << "Choose a main function:" <<endl;
     for(int i = 0; i < k; ++i)
@@ -40,8 +38,9 @@ int main(int argc, char const *argv[])
 int static_sort_main()
 {
     using namespace my::static_alg;
-    using Unsorted = Seq<6, 5, 8, 7, 9, 2, 3, 1, 11>;
-    using Sorted = sort_t<Unsorted>;
+//	using Unsorted = Seq<6, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3, 1, 11, 5, 8, 7, 9, 2, 3>;
+ 	using Unsorted = Seq<1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20>;
+	using Sorted = sort_t<Unsorted>;
     print(Unsorted{});
     print(Sorted{});
     return 0; 
@@ -51,43 +50,35 @@ int static_sort_main()
 int my_tuple_main()
 {
     using namespace std;
-    constexpr my::tuple<int, char, int>
-        tup1(0, 'a', -11);
+    my::tuple<int, char, int>
+        tup1(5, 'a', -11);
+    my::tuple<int, int, int, int, int, int>
+        tup2(-2, -1, 0, 1, 2, 3);
+    my::tuple<bool, const char*, void*, size_t>
+        tup3(true, "i am tup3", nullptr, 1);
+    my::tuple<bool, const char*, void*, size_t>
+        tup4; //default construct
+    auto 
+        tup5 = my::make_tuple("hello","tup5",'a','b');
 
-    return get<0>(tup1);
+    tup3.set<2>(&tup3); //explicit set
+    tup4 = tup3; //copy assign
+    tup3.template get<3>() += 1; //object function call to lvalue access and re-assign
+    my::get<2>(tup4) = &tup4; //lvalue access and re-assign
+    tup4.set<1>("i am tup4");
+    const char* some_text = "some_text";
+
+    cout <<
+        boolalpha <<
+        "rval tuple " << my::tuple<int,bool>(1234, true) <<
+        "make_tuple " << my::make_tuple(543, 'x', "foo") <<
+        "tup1 " << tup1 <<
+        "tup2 " << tup2 <<
+        hex <<
+        "tup3 " << tup3 <<
+        "tup4 " << tup4 <<
+        "tup5 " << tup5 <<
+    endl;
+
+    return 0;
 }
-// int my_tuple_mainx()
-// {
-//     using namespace std;
-//     my::tuple<int, char, int>
-//         tup1(5, 'a', -11);
-//     my::tuple<int, int, int, int, int, int>
-//         tup2(-2, -1, 0, 1, 2, 3);
-//     my::tuple<bool, const char*, void*, size_t>
-//         tup3(true, "i am tup3", nullptr, 1);
-//     my::tuple<bool, const char*, void*, size_t>
-//         tup4; //default construct
-//     auto 
-//         tup5 = my::make_tuple("hello","tup5",'a','b');
-
-//     tup3.set<2>(&tup3); //explicit set
-//     tup4 = tup3; //copy assign
-//     tup3.template get<3>() += 1; //object function call to lvalue access and re-assign
-//     my::get<2>(tup4) = &tup4; //lvalue access and re-assign
-//     tup4.set<1>("i am tup4");
-//     const char* some_text = "some_text";
-
-//     cout <<
-//         boolalpha <<
-//         "rval tuple " << my::tuple<int,bool>(1234, true) <<
-//         "make_tuple " << my::make_tuple(543, 'x', "foo") <<
-//         "tup1 " << tup1 <<
-//         "tup2 " << tup2 <<
-//         hex <<
-//         "tup3 " << tup3 <<
-//         "tup4 " << tup4 <<
-//         "tup5 " << tup5 <<
-//     endl;
-
-//     return 0;
-// }
