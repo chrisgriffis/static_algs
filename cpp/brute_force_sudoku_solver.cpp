4197c6d6b98f22c79191ee3bd7b175d8e6024214
chrisg <2273415+chrisgriffis@users.noreply.github.com>
2021-07-02 16:39:37 -0700

    Update brute_force_sudoku_solver.cpp
    removed superfluous vector<char> specifier in grid list-init

--- cpp/brute_force_sudoku_solver.cpp
+++ cpp/brute_force_sudoku_solver.cpp
@@ -1,3 +1,32 @@
+/*
+sudoku solver example in modern C++
+https://github.com/chrisgriffis/static_algs/cpp/brute_force_sudoku_solver.cpp
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+Copyright (c) 2012-2021 Chris Griffis <Chris.Griffis@outlook.com>
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* this is not a static alg. the original motivation of this repo was inspired
+by solving the problem of determining latin square subsquare array bounds at 
+compiletime, which lead me down a TMP rabbit hold of compile time integer sqrt, 
+which lead me into this place to keep stuff.
+*/
+
 #include <iostream>
 #include <vector>
 #include <bitset>
@@ -111,17 +140,17 @@ int main()
 {
     grid puz1 =
     {
-        vector<char>{'.','.','.',   '.','5','.',    '.','.','8'},
-        vector<char>{'.','.','.',   '.','.','7',    '5','6','.'},
-        vector<char>{'4','2','.',   '.','.','.',    '.','.','.'},
+        {'.','.','.',   '.','5','.',    '.','.','8'},
+        {'.','.','.',   '.','.','7',    '5','6','.'},
+        {'4','2','.',   '.','.','.',    '.','.','.'},
 
-        vector<char>{'.','.','.',   '.','.','.',    '1','.','.'},
-        vector<char>{'.','5','.',   '2','.','6',    '.','.','.'},
-        vector<char>{'.','.','1',   '.','8','3',    '.','2','4'},
+        {'.','.','.',   '.','.','.',    '1','.','.'},
+        {'.','5','.',   '2','.','6',    '.','.','.'},
+        {'.','.','1',   '.','8','3',    '.','2','4'},
 
-        vector<char>{'3','7','.',   '4','9','5',    '.','.','.'},
-        vector<char>{'.','.','.',   '.','.','.',    '.','.','.'},
-        vector<char>{'6','.','.',   '.','.','.',    '3','.','.'},
+        {'3','7','.',   '4','9','5',    '.','.','.'},
+        {'.','.','.',   '.','.','.',    '.','.','.'},
+        {'6','.','.',   '.','.','.',    '3','.','.'},
     };
     
     size_t row = 0;
