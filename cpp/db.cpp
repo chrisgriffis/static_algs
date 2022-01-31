79151d10e092b1efb4bbb3d5d4d7b06b96678c9b
Chris Griffis <chris.griffis@microsoft.com>
2022-01-30 22:31:16 -0800

    sdfb

--- cpp/db.cpp
+++ cpp/db.cpp
@@ -7,91 +7,68 @@
 #include <type_traits>
 using namespace std;
 
-template<typename... field_types>
-struct table_spec;
+//template<typename String, template <typename,template <typename...> typename,typename...> typename tspec, typename... field_types>
+//template<typename String, template <typename...> typename tspec, typename... field_types>
+template<typename, typename...> 
+struct basic_table_spec;
 
-
-template<typename String, template <typename...> typename table_spec, typename... field_types>
-struct Record
-    : public table_spec<String, field_types...>
+template<typename String, template <typename...> typename basic_table_spec, typename... field_types>
+class basic_record : basic_table_spec<String, field_types...>
 {
-    using table_spec_t = table_spec<String, field_types...>;
-
-    table_spec_t::fields_t m_fields;
-
-    Record() = delete;
-
-//protected:
-    //with args
-    template<typename String2, typename... field_types_ctor>
-        Record(table_spec<String2, field_types_ctor...> schema, field_types_ctor... fields) :
-        table_spec_t{ schema },
-        m_fields{ std::make_tuple(fields...) }
-    {}
-    //with tup
-    template<typename String2, typename... field_types_ctor>
-        Record(table_spec<String2, field_types_ctor...> schema, std::tuple<field_types_ctor...> tup) :
-        table_spec<String2, field_types_ctor...>( schema ),
-        m_fields{ tup }
-    {}
-    //no args
-    template<typename String2, typename... field_types_ctor>
-        Record(table_spec<String2, field_types_ctor...> schema) :
-        table_spec<String2, field_types_ctor...>{ schema },
-        m_fields{  }
-    {}
-    //another rec
-        Record(const Record& rec) :
-        table_spec<String, field_types...>{ rec.m_column_headings },
-        table_spec<String, field_types...>::m_fields{ rec.m_fields }
-    {}
+    basic_record() = delete;
 public:
+    using fields_t = std::tuple<field_types...>;
+
+    std::tuple<field_types...> m_fields;
+
     //with args
-    static Record create(field_types... fields)
-    { return Record(std::forward<field_types>(fields)...);}
+    template<typename String2, typename... field_types_ctor>
+    basic_record(
+        basic_table_spec<String2, basic_record, field_types_ctor...> schema, 
+        field_types_ctor... fields) 
+        :   basic_table_spec<String, field_types_ctor...>(schema),
+            m_fields{ std::make_tuple(fields...) }
+    {}
     //with tup
-    /*template<typename... field_types_ctor,
-        typename = std::enable_if_t<sizeof...(field_types) == sizeof...(field_types_ctor)>>
-        create(table_spec_t schema, std::tuple<field_types_ctor...> tup) :
-        table_spec<field_types_ctor...>{ schema },
-        m_fields{ tup }
+    template<typename String2, typename... field_types_ctor>
+    basic_record(
+        basic_table_spec<String2, basic_record,field_types_ctor...> schema, 
+        std::tuple<field_types_ctor...> tup)
+        :   basic_table_spec<String2, field_types_ctor...>(schema),
+            m_fields{ tup }
     {}
     //no args
-    template<typename... field_types_ctor,
-        typename = std::enable_if_t<sizeof...(field_types) == sizeof...(field_types_ctor)>>
-        create(table_spec_t schema) :
-        table_spec<field_types_ctor...>{ schema },
-        m_fields{  }
+    template<typename String2, typename... field_types_ctor>
+    basic_record(
+        basic_table_spec<String2, basic_record, field_types_ctor...> schema)
+        :   basic_table_spec<String2, field_types_ctor...>(schema),
+            m_fields{  }
     {}
-    //another rec
-    template<typename... field_types_ctor,
-        typename = std::enable_if_t<sizeof...(field_types) == sizeof...(field_types_ctor)>>
-        create(const Record< table_spec<String, field_types...>, field_types_ctor...>& rec) :
-        table_spec<String, field_types...>{ rec.m_column_headings },
-        m_fields{ rec.m_fields }
-    {}*/
+    //from another rec
+    basic_record(const basic_record& rec) :
+        basic_table_spec<String, basic_record, field_types...>{ rec.m_column_headings },
+        basic_table_spec<String, basic_record, field_types...>::m_fields{ rec.m_fields }
+    {}
 
-    Record& operator=(tuple<field_types...>& tup)
+    basic_record& operator=(tuple<field_types...>& tup)
     {
         m_fields = tup; return *this;
     }
-    Record& operator=(Record& rec) = delete;
-    constexpr std::map<const char*, std::string> to_lookup()
+    basic_record& operator=(basic_record& rec) = delete;
+    constexpr std::map<String, String> to_lookup()
     {
         return to_lookup(std::index_sequence_for<field_types...>{});
     }
     //from args
-    Record to_record(field_types... fields) override
+    basic_record to_record(field_types... fields) 
     {
-        return Record(*this, fields...);
+        return basic_record(*this, fields...);
     }
 
-
-
     //from tup
-    Record to_record(std::tuple<field_types...> tup) override
+    basic_record to_record(std::tuple<field_types...> tup) 
     {
-        return Record(*this, tup);
+        return basic_record(*this, tup);
     }
 
 private:
@@ -100,42 +77,47 @@ private:
     {
         return std::map<const char*, std::string>{
             {
-                table_spec_t::m_column_names[Is],
+                basic_table_spec<String, field_types...>::m_column_names[Is],
                     [this] { stringstream ss; ss << std::get<Is>(m_fields); return ss.str(); }()
             }... };
     }
 };
 
+
 template <typename...> struct L;
-template <typename T> 
+template <typename T>
 struct L<T>
 {
     using type = T;//std::enable_if_t<(std::is_same_v<T,Ts> && ...),T>;
 };
-template <typename T, typename... Ts> 
-struct L<T,Ts...>
+template <typename T, typename... Ts>
+struct L<T, Ts...>
 {
     using type = T;//std::enable_if_t<(std::is_same_v<T,Ts> && ...),T>;
 };
-template< typename String, typename... field_types>
-struct table_spec<String, field_types...>
+//template<typename String, template <typename...> typename record_t, typename... field_types>
+//template<typename String, template <typename,template <typename...> typename,typename...> typename record_t, typename... field_types>
+template<typename String, typename... field_types>
+struct basic_table_spec
 {
-    using record_t = Record<String, table_spec, field_types...>;
-    using fields_t = std::tuple<field_types...>;
+    using fields_t = std::tuple<field_types...>;//typename record_t::fields_t;
     using column_names_t = const std::array<String, sizeof...(field_types)>;
 
     std::array<String, sizeof...(field_types)> m_column_names;
 
     template<template <typename...> typename L, typename... Strings>
-        table_spec( L<Strings...>, Strings... strs )
+    basic_table_spec(L<Strings...>, Strings... strs)
         : m_column_names{ strs... }
     {}
-    table_spec( const table_spec& t )
+public:
+    basic_table_spec(const basic_table_spec& t)
         : m_column_names{ t.m_column_names }
     {}
     template<typename... Strings>
     static auto create(Strings... strs)
-    { return table_spec<typename L<Strings...>::type, field_types...>(L<Strings...>{}, strs...); }
+    {
+        return basic_table_spec(L<Strings...>{}, strs...);
+    }
 
     template<typename C>
     constexpr std::string to_list(C c, std::string separator = ",")
@@ -144,23 +126,6 @@ struct table_spec<String, field_types...>
         std::stringstream s;
         for (unsigned int n = 0; n < N; ++n) { s << c[n]; if (N != n + 1) { s << separator; } }
         return s.str();
-    }
-    //from args
-    virtual record_t to_record(field_types... fields)
-    {
-        return record_t(*this, fields...);
-    }
-
-    //no args
-    virtual record_t to_record()
-    {
-        return record_t(*this);
-    }
-
-    //from tup
-    virtual record_t to_record(std::tuple<field_types...> tup)
-    {
-        return record_t(*this, tup);
     }
 
     constexpr std::string column_headings_list()
@@ -172,10 +137,44 @@ struct table_spec<String, field_types...>
         return to_list(std::vector<char>(sizeof...(field_types), placeholder));
     }
 };
+//from args
+template<typename String, template <typename...> typename basic_table_spec, typename... field_types>
+basic_record<String, basic_table_spec, field_types...> to_record(
+    basic_table_spec<String, field_types...> ts,
+    field_types... fields)
+{
+    return basic_record<String, basic_table_spec, field_types...>(ts, fields...);
+}
+
+//no args
+template<typename String, template <typename...> typename basic_table_spec, typename... field_types>
+decltype(auto) to_record(
+    basic_table_spec<String, field_types...> ts)
+{
+    return basic_record<String, basic_table_spec, field_types...>(ts);
+}
+
+//from tup
+template<typename String, template <typename...> typename basic_table_spec, typename... field_types>
+decltype(auto) to_record(
+    basic_table_spec<String, field_types...> ts, 
+    std::tuple<field_types...> tup)
+{
+    return basic_record<String, basic_table_spec, field_types...>(ts, tup);
+}
+
+template<typename... field_types>
+using record_cstr = basic_record<const char*, basic_table_spec, field_types...>;
+template<typename... field_types>
+using record = basic_record<std::string, basic_table_spec, field_types...>;
+template<typename... field_types>
+using table_spec_cstr = basic_table_spec<const char*, field_types...>;
+template<typename... field_types>
+using table_spec = basic_table_spec<std::string, field_types...>;
 
 
-template<typename... Ts>
-std::ostream& operator<<(std::ostream& s, Record<Ts...>  rec)
+template<typename String, template <typename...> typename tspec, typename... field_types>
+std::ostream& operator<<(std::ostream& s, basic_record<String, tspec, field_types...>  rec)
 {
     auto l = rec.to_lookup();
     for_each(l.begin(), l.end(), [&](auto e) {s << e.second; });
@@ -187,7 +186,10 @@ int main() {
     const char* id = "id";
     const char* name = "name";
     const char* type = "type";
-    auto s = table_spec<const char*, int, const char*, const char*>::template create(id, name, type);
-    cout << s.column_headings_list();
+    //auto s = table_spec<int, const char*, const char*>::create(id, name, type);
+    auto s = table_spec<int, const char*, const char*>::create("id", "name", "type");
+    cout << s.column_headings_list() << endl;
+    auto r = to_record(s, 9, "qwer", "dtyj");
+    cout << r << endl;
     return 0;
 }
