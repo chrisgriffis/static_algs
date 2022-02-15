8857815c6403721b223e77355a3fdf126de7d50b
chrisg <2273415+chrisgriffis@users.noreply.github.com>
2022-02-15 10:58:34 -0800

    add commenting and new permalink to wandbox

--- typesafe_db_row_schema/demo.cpp
+++ typesafe_db_row_schema/demo.cpp
@@ -1,6 +1,15 @@
-//https://wandbox.org/permlink/df8lYQSB7tzbp0Ch
+//https://wandbox.org/permlink/OVr11fSypfhV41tR
 #include <iostream>
 #include "db_types.h"
+
+/*
+create a database table schema by specifying designated native types for field values, 
+such that a given native type is restricted to those with an extant 
+type-value-conversion-to-database-value-type. any non conformities create a compile 
+error. database row records can only be created as splinters of an existing schema 
+definition and type integrity is enforced. built in utilities allow serialization 
+to output streams.
+*/
 
 template<typename... f_types>
 using db_row_schema = basic_row_schema<single_record, meta::is_type::supported, std::string, f_types...>;
@@ -8,7 +17,7 @@ using db_row_schema = basic_row_schema<single_record, meta::is_type::supported, 
 int demo(std::ostream& sout) {
 
     auto DB_SCHEMA_PHOTOS = db_row_schema<
-        std::string,// path;
+        std::string,    // path;
         winrt::uint64_t,// folderKey;
         winrt::uint64_t,// thumbnailCacheId;
         winrt::uint32_t,// fileAttributes;
@@ -28,11 +37,11 @@ int demo(std::ostream& sout) {
         "Width",
         "Height");
 
-    sout << "DB_SCHEMA_PHOTOS col headings: " << endl << DB_SCHEMA_PHOTOS.headings_as_list() << endl << endl;
-    sout << "DB_SCHEMA_PHOTOS to_single_record, empty: " << endl << DB_SCHEMA_PHOTOS.to_single_record() << endl << endl;
-    auto rec = DB_SCHEMA_PHOTOS.to_single_record("a/cool/path", 7, 3, 67, 23, 4, 6, 45, 7);
-    sout << "DB_SCHEMA_PHOTOS to_single_record, with values: " << endl << rec << endl << endl;
-    sout << "DB_SCHEMA_PHOTOS to_single_record, converted values: " << endl << rec.converted_row_values() << endl << endl;
+    sout << "DB_SCHEMA_PHOTOS col headings: " << endl <<  DB_SCHEMA_PHOTOS.headings_as_list() << endl << endl ;
+    sout << "DB_SCHEMA_PHOTOS to_single_record, empty: " << endl <<  DB_SCHEMA_PHOTOS.to_single_record() << endl << endl ;
+    auto rec = DB_SCHEMA_PHOTOS.to_single_record("a/cool/path",7,3,67,23,4,6,45,7);
+    sout << "DB_SCHEMA_PHOTOS to_single_record, with values: " << endl << rec  << endl << endl ;
+    sout << "DB_SCHEMA_PHOTOS to_single_record, converted values: " << endl <<  rec.converted_row_values() << endl << endl ;
 
     return 0;
 }
